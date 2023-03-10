9b94fa118dd6498c9b9815e6efc1972cbcb549c4
priyakll <91473166+priyakll@users.noreply.github.com>
2023-03-10 12:56:49 +0530

    Update AddTransaction.js

--- client/public/src/components/AddTransaction.js
+++ client/public/src/components/AddTransaction.js
@@ -1,1 +1,39 @@
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const AddTransaction = ({ addTransaction }) => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const newTransaction = {
+      id: uuidv4(),
+      text,
+      amount: +amount
+    };
+    addTransaction(newTransaction);
+    setText('');
+    setAmount('');
+  }
+
+  return (
+    <div>
+      <h3>Add new transaction</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-control">
+          <label htmlFor="text">Text</label>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
+        </div>
+        <div className="form-control">
+          <label htmlFor="amount">Amount <br /> (negative - expense, positive - income)</label>
+          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+        </div>
+        <button className="btn">Add transaction</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddTransaction;
 
